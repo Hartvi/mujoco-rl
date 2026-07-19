29a4688c1b3a99c1ec45a97c8603a8109b15ed81
Hartvi <j.hartvich@gmail.com>
2026-07-19 16:53:59 +0200

    update headless training script with a longer episode

--- train_headless.sh
+++ train_headless.sh
@@ -1,1 +1,1 @@
-/home/hartvi/venvs/ros2-torch/bin/python train_pincer_mlp.py     --updates 100     --horizon 512     --log-file rewards_headless.csv
+/home/hartvi/venvs/ros2-torch/bin/python train_pincer_mlp.py     --updates 1000     --horizon 1512     --log-file rewards_headless.csv  --episode-max-steps 500
